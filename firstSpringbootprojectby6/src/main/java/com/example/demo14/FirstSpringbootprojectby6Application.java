f0e557a852bd01479e90e63076efffb067a55d2e
Anoj1414 <94505304+Anoj1414@users.noreply.github.com>
2023-01-09 05:18:45 -0800

    Add files via upload

---
+++ firstSpringbootprojectby6/src/main/java/com/example/demo14/FirstSpringbootprojectby6Application.java
@@ -0,0 +1,13 @@
+package com.example.demo14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootprojectby6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootprojectby6Application.class, args);
+	}
+
+}
---
+++ firstSpringbootprojectby6/src/test/java/com/example/demo14/FirstSpringbootprojectby6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringbootprojectby6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
