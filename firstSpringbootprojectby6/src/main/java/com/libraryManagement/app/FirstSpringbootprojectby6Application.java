3e5be11e6633cc28132be391f2a85392b5fa1142
jarvis5797 <ashutosh.baranwal@acsicorp.com>
2023-01-10 12:12:28 +0530

    created-pacakges

--- firstSpringbootprojectby6/src/main/java/com/libraryManagement/app/FirstSpringbootprojectby6Application.java
+++ firstSpringbootprojectby6/src/main/java/com/libraryManagement/app/FirstSpringbootprojectby6Application.java
@@ -1,4 +1,4 @@
-package com.example.demo14;
+package com.libraryManagement.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
