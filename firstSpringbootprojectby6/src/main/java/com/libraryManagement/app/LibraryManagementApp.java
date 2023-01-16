f57ebd36cd15d15d7b4ffab6904ca3018b3a64ee
jarvis5797 <ashutosh.baranwal@acsicorp.com>
2023-01-16 10:59:46 +0530

    changed project main class name

--- firstSpringbootprojectby6/src/main/java/com/libraryManagement/app/LibraryManagementApp.java
+++ firstSpringbootprojectby6/src/main/java/com/libraryManagement/app/LibraryManagementApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FirstSpringbootprojectby6Application {
+public class LibraryManagementApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstSpringbootprojectby6Application.class, args);
+		SpringApplication.run(LibraryManagementApp.class, args);
 	}
 
 }
